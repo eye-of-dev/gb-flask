50a1c29bae94983525225102056f1da90edb0881
Anton Lisikov <anton.lisikov@gmail.com>
2021-05-21 22:19:48 +0300

    Урок 3. Авторизация пользователя и начало работы с базой данных. SQLAlchemy

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,31 +1,28 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
+from flask_login import login_required
+from werkzeug.exceptions import NotFound
+
+from blog.users.models import Users
 
 users = Blueprint('users', __name__, url_prefix='/users', static_folder='../static')
-
-USERS = {
-    1: 'Романов Денис Игоревич',
-    2: 'Семенов Кирилл Андреевич',
-    3: 'Матвеев Максим Филиппович',
-    4: 'Орлов Марсель Максимович',
-    5: 'Масленникова Екатерина Ивановна',
-}
 
 
 @users.route('/')
 def users_list():
+    users = Users.query.all()
     return render_template(
         'users/list.html',
-        users=USERS
+        users=users
     )
 
 
 @users.route('/<int:pk>/')
+@login_required
 def users_info(pk: int):
+    user = Users.query.filter_by(id=pk).one_or_none()
 
-    try:
-        user = USERS[pk]
-    except KeyError:
-        return redirect('/users/')
+    if user is None:
+        raise NotFound(f'User #{pk} does not exist!')
 
     return render_template(
         'users/detail.html',
