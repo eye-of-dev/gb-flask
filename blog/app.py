538ec943df52f98c95e77d880f6932c43056f053
Anton Lisikov <anton.lisikov@gmail.com>
2021-05-28 20:51:39 +0300

    fix: fixed quotes

--- blog/app.py
+++ blog/app.py
@@ -10,8 +10,8 @@ from blog.users.views import users
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'o)^xsrh#7(7&n1r+62!vz*qp+ih=lhem5_i%j_#o7wu-5&7#y*'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gb-flask:secret@localhost/gb-flask"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gb-flask:secret@localhost/gb-flask'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     login_manager.init_app(app)
     Migrate(app, db)
