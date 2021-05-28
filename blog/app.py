129a7be1667e693a8374b30a441d329aba65a68a
Anton Lisikov <anton.lisikov@gmail.com>
2021-05-28 20:42:15 +0300

    Урок 4. Docker, docker-compose, Postgres. Миграции схем Flask-Migrate и alembic

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from blog.articles.views import articles
 from blog.auth.views import auth, login_manager
@@ -9,10 +10,11 @@ from blog.users.views import users
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'o)^xsrh#7(7&n1r+62!vz*qp+ih=lhem5_i%j_#o7wu-5&7#y*'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gb-flask:secret@localhost/gb-flask"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     login_manager.init_app(app)
+    Migrate(app, db)
 
     register_blueprints(app)
     return app
